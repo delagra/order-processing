78643d859f425953de4b5f7a1e424e526b7c7452
Vlade Shestakov <vlade@delagra.com>
2023-07-06 08:25:43 -0400

    added personalization and -02 check

--- main.py
+++ main.py
@@ -8,6 +8,16 @@ def extract_color_finish(string):
     if match:
         if match.span():
             return match.group(2)
+def extract_personalization(string):
+    match = re.search(r'Personalization\s+(.*)', string, re.IGNORECASE)
+    if match:
+        return match.group(1)
+
+def check_02(string):
+    match = re.search(r'-02$', string, re.IGNORECASE)
+    if match:
+        return "Cambria"
+
 #add columns
 df['Font'] = ""
 df['Letter 1'] = ""
@@ -23,7 +33,9 @@ for index, row in df.iterrows():
     if row['Order Number'] > 1000000000:
         #extract Color/Finish
         df.at[index,'Color'] = extract_color_finish(row['Item Name'])
-        print(row['Order Number'])
-        #extract Personalization
-        #check SKU "-02"
-print(df)
+        # extract Personalization
+        df.at[index, 'Text 1'] = extract_personalization(row['Item Name'])
+        # check SKU "-02"
+        df.at[index, 'Font'] = check_02(row['SKU'])
+
+print(df['Font'])
