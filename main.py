f472064cf15bd3c81b1bc11caf3f6eeb182c760e
Vlade Shestakov <vlade@delagra.com>
2023-07-06 15:19:47 -0400

    added the remaining functions and output file

--- main.py
+++ main.py
@@ -18,6 +18,52 @@ def check_02(string):
     if match:
         return "Cambria"
 
+def get_color(string):
+    match = re.search(r"(Color:\s+)(\w+)", string, re.IGNORECASE)
+    if match:
+        if match.span():
+            return match.group(2)
+def get_name(string):
+    match = re.search(r"Name: \(Please limit to one word\)(.*?)Choose", string, re.IGNORECASE)
+    if match:
+        if match.span():
+            return match.group(1)
+def get_text(string):
+    match = re.search(r"Custom Text:(.*?)Choose", string, re.IGNORECASE)
+    if match:
+        if match.span():
+            return match.group(1)
+def get_top(string):
+    match = re.search(r"\(Top\):(.*?)Choose", string, re.IGNORECASE)
+    if match:
+        if match.span():
+            return match.group(1)
+def get_center(string):
+    match = re.search(r"\(Center\):(.*?)Choose", string, re.IGNORECASE)
+    if match:
+        if match.span():
+            return match.group(1)
+def get_bottom(string):
+    match = re.search(r"\(Bottom\):(.*?)Choose", string, re.IGNORECASE)
+    if match:
+        if match.span():
+            return match.group(1)
+
+def get_font(string):
+    match = re.search(r"(Choose your Font:\s+)(\w+)", string, re.IGNORECASE)
+    if match:
+        if match.span():
+            return match.group(2)
+def get_comment(string):
+    match = re.search(r"Customer Note:(.*?)_customily", string, re.IGNORECASE)
+    if match:
+        if match.span():
+            return match.group(1)
+def get_letter(string):
+    match = re.search(r"Letter:\s+(\S)", string, re.IGNORECASE)
+    if match:
+        if match.span():
+            return match.group(1)
 #add columns
 df['Font'] = ""
 df['Letter 1'] = ""
@@ -37,5 +83,13 @@ for index, row in df.iterrows():
         df.at[index, 'Text 1'] = extract_personalization(row['Item Name'])
         # check SKU "-02"
         df.at[index, 'Font'] = check_02(row['SKU'])
-
-print(df['Font'])
+    if row['Order Number'] < 1000000000:
+        df.at[index, 'Color'] = get_color(row['Item Name'])
+        text1 =get_name(row['Item Name']) or get_text(row['Item Name']) or get_top(row['Item Name'])
+        df.at[index, 'Text 1'] = text1
+        df.at[index, 'Font'] = get_font(row['Item Name'])
+        df.at[index, 'Customer Comments'] = get_comment(row['Item Name'])
+        df.at[index, 'Letter'] = get_letter(row['Item Name'])
+        df.at[index, 'Text 2'] = get_center(row['Item Name'])
+        df.at[index, 'Text 3'] = get_bottom(row['Item Name'])
+df.to_excel('output.xlsx')
